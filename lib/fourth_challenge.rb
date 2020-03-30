3a2ace66066a8e0afd620a24fe525edff564a59f
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-30 19:24:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,7 +24,10 @@ def fourth_challenge
         age: "30",
         attitude: "confused"
         }
-        {
+        {name: "Nurse",
+        age: "44",
+        attitude: "worried"
+        }
       ]
    }
 }
